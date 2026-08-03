731fb59e20c216ff79df47108e7b31933743727d
Filip Stanciu <filipstanciu2005@gmail.com>
2026-08-03 15:28:27 +0300

    Expose chat endpoint through FastAPI

--- api/main.py
+++ api/main.py
@@ -1,13 +1,35 @@
 from fastapi import FastAPI
 
+from api.schemas import ChatRequest, ChatResponse
+from app.chatbot import SmartLibrarian
+
 app = FastAPI(
     title="Smart Librarian API",
+    description="AI Book Recommendation API",
     version="1.0.0",
 )
 
+chatbot = SmartLibrarian()
+
 @app.get("/")
-def home():
+def root():
 
     return {
-        "message": "Smart Librarian API is running."
+        "message": "Smart Librarian API is running!"
     }
+    
+@app.post(
+    "/chat",
+    response_model=ChatResponse,
+)
+def chat(
+    request: ChatRequest,
+):
+
+    response = chatbot.chat(
+        request.query
+    )
+
+    return ChatResponse(
+        response=response
+    )
